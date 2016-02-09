397913325f6682c1154e785e10be5cb714e1dbf3
Akriti <sehrawat@usc.edu>
2016-02-09 14:33:09 -0800

    application js changes

--- renas_code/js/application-script.js
+++ renas_code/js/application-script.js
@@ -4,8 +4,6 @@ $('.navigation li:nth-child(1)').click(function() {
     document.getElementById("app2").style.display = "none";
     document.getElementById("app3").style.display = "none";
     document.getElementById("app4").style.display = "none";
-    document.getElementById("app5").style.display = "none";
-    document.getElementById("app6").style.display = "none";
 });
 
 $('.navigation li:nth-child(2)').click(function() {
@@ -14,8 +12,6 @@ $('.navigation li:nth-child(2)').click(function() {
     document.getElementById("app2").style.display = "block";
     document.getElementById("app3").style.display = "none";
     document.getElementById("app4").style.display = "none";
-    document.getElementById("app5").style.display = "none";
-    document.getElementById("app6").style.display = "none";
 });
 
 $('.navigation li:nth-child(3)').click(function() {
@@ -24,8 +20,6 @@ $('.navigation li:nth-child(3)').click(function() {
     document.getElementById("app2").style.display = "none";
     document.getElementById("app3").style.display = "block";
     document.getElementById("app4").style.display = "none";
-    document.getElementById("app5").style.display = "none";
-    document.getElementById("app6").style.display = "none";
 });
 
 $('.navigation li:nth-child(4)').click(function() {
@@ -34,26 +28,5 @@ $('.navigation li:nth-child(4)').click(function() {
     document.getElementById("app2").style.display = "none";
     document.getElementById("app3").style.display = "none";
     document.getElementById("app4").style.display = "block";
-    document.getElementById("app5").style.display = "none";
-    document.getElementById("app6").style.display = "none";
 });
 
-$('.navigation li:nth-child(5)').click(function() {
-    //alert("clicked fifth");
-    document.getElementById("app1").style.display = "none";
-    document.getElementById("app2").style.display = "none";
-    document.getElementById("app3").style.display = "none";
-    document.getElementById("app4").style.display = "none";
-    document.getElementById("app5").style.display = "block";
-    document.getElementById("app6").style.display = "none";
-});
-
-$('.navigation li:nth-child(6)').click(function() {
-    //alert("clicked sixth");
-    document.getElementById("app1").style.display = "none";
-    document.getElementById("app2").style.display = "none";
-    document.getElementById("app3").style.display = "none";
-    document.getElementById("app4").style.display = "none";
-    document.getElementById("app5").style.display = "none";
-    document.getElementById("app6").style.display = "block";
-});
